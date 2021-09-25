5c56c57e2b4c49e770a08cf7fd6fec9cdb9afa50
Maliarte <42978269+Maliarte@users.noreply.github.com>
2021-09-25 15:10:23 -0300

    Add files via upload
    Desenvolvimento JS

---
+++ js/arquivo.js
@@ -0,0 +1,4 @@
+
+// código javascript
+/* aqui eu posso colocar qualquer comando JS */
+alert("Alo mundo!");
---
+++ js/work.js
@@ -0,0 +1,12 @@
+/*Utilizando os conceitos aprendidos até agora, resolva o exercício:
+Construa um script em JavaScript que permita o usuário introduzir:
+• Nome;
+• Idade;
+• Gasto diário com locomoção: (em R$);
+• Tempo semanal de estudo extraclasse (em h);
+• Tempo semanal livre (em h);
+• Mostre seus dados; e
+• Relação do tempo livre com o tempo total: tempo livre*100/(tempo livre +
+tempo estudo)
+E apresente numa página HTML
+*/
